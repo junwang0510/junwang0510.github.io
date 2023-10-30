1aade26d891906cb1c7623e6dcd4646104cacfa0
Jun Wang <junw3@uw.edu>
2023-10-30 02:33:15 -0700

    removed dark theme

--- scripts.js
+++ scripts.js
@@ -6,14 +6,3 @@ function showEmail() {
     }
     emailDiv.classList.toggle("visible");
 }
-
-function toggleTheme() {
-    const body = document.body;
-    if (body.classList.contains('dark-theme')) {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-    } else {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-    }
-}
