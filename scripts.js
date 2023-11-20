65b44b53ca56289c10c843f2843055fcfd1adba2
Jun Wang <junwang@Juns-MacBook-Pro.local>
2023-11-19 23:46:53 -0800

    reload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function showEmail() {
     var emailDiv = document.getElementById("email-display");
     if (emailDiv.childNodes.length === 0) {
-        var emailText = document.createTextNode("Email: junw3 [AT] cs.washington.edu");
+        var emailText = document.createTextNode("Email: junwang0510 [AT] gmail [DOT] com");
         emailDiv.appendChild(emailText);
     }
     emailDiv.classList.toggle("visible");
