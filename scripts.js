e721fea99e9573932724f03e5e9627e158ff5e17
Jun Wang <junwang@Juns-MacBook-Pro.local>
2023-11-21 15:43:01 -0800

    recent favorites

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function showEmail() {
     var emailDiv = document.getElementById("email-display");
     if (emailDiv.childNodes.length === 0) {
-        var emailText = document.createTextNode("Email: junwang0510 [AT] gmail [DOT] com");
+        var emailText = document.createTextNode("Email: junw3 [AT] cs.washington.edu");
         emailDiv.appendChild(emailText);
     }
     emailDiv.classList.toggle("visible");
