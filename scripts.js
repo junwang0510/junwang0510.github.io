a7d09f899eccc9dc01acf764d71e6703385dd821
junwang0510 <junwang0510@hotmail.com>
2023-12-20 09:32:17 -0800

    removed email animations

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function showEmail() {
-    var emailDiv = document.getElementById("email-display");
-    if (emailDiv.childNodes.length === 0) {
-        var emailText = document.createTextNode("Email: junw3 [AT] cs.washington.edu");
-        emailDiv.appendChild(emailText);
-    }
-    emailDiv.classList.toggle("visible");
-}
